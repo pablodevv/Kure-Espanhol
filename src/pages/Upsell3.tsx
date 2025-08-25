85c7a24701e696d3b61ff290195047891eefded3
Pablo Rodriguez <79975765+pablodevv@users.noreply.github.com>
2025-08-25 07:09:02 -0300

    Update Upsell3.tsx

--- src/pages/Upsell3.tsx
+++ src/pages/Upsell3.tsx
@@ -6,14 +6,13 @@ import { useNavigate } from "react-router-dom"
 
 
 export default function UpsellMenteBlindada() {
-  const [timeLeft, setTimeLeft] = useState(600) // 10 minutos
+  const [timeLeft, setTimeLeft] = useState(600)
   const navigate = useNavigate()
 
 
 
   
 useEffect(() => {
-  // Rola a página para o topo sempre que ela for renderizada
   window.scrollTo(0, 0);
 }, []);
   
@@ -32,7 +31,7 @@ useEffect(() => {
   }
 
   const handleAccept = () => {
-    window.location.href = "https://pay.kirvano.com/3ecc476b-077e-4c6c-8fcd-3528821e2dc6" // substitua pelo seu link real
+    window.location.href = "https://pay.kirvano.com/3ecc476b-077e-4c6c-8fcd-3528821e2dc6" 
   }
 
   const handleDecline = () => {
